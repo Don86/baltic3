e95f7c9781b89ee1b2b3cd551efbdb841a761155
Don86 <andreyito90@gmail.com>
2018-01-12 19:15:14 +1100

    Added preliminary biophylo_utils and example

--- biophylo_utils.py
+++ biophylo_utils.py
@@ -1,7 +1,6 @@
-import re
-import copy
-import math
-import datetime as dt
+import time
+import numpy as np
+import pandas as pd
 
 from Bio import Phylo
 
@@ -9,3 +8,86 @@ from Bio import Phylo
 A bunch of commonly used utility functions related to Bio.Phylo. Not strictly
 related to baltic3, but I'm putting them here for git saving convenience.
 """
+
+def tip_to_tip_distance(my_tree, tip1, tip2):
+    """Computes the tip-to-mrca-to-tip distance between tip1 and tip2.
+
+    PARAMS
+    ------
+    my_tree: biopython tree object.
+    tip1, tip2: biopython Clade objects
+
+    RETURNS
+    -------
+    tt_dist: float. tip-to-mrca-to-tip distance between tip1 and tip2.
+    """
+
+    mrca = my_tree.common_ancestor(tip1, tip2)
+    t1_mrca_dist = my_tree.distance(tip1, mrca)
+    t2_mrca_dist = my_tree.distance(tip2, mrca)
+
+    tt_dist = t1_mrca_dist + t2_mrca_dist
+    return tt_dist
+
+
+def get_clade_labels(my_tree, ref_names_ls, verbose=True):
+    """Computes tip-to-tmrc-to-tip distances for each tipname in my_tree, to
+    each reference name in ref_names_ls. my_tree must contain the reference
+    names in ref_names_ls.
+
+    PARAMS
+    ------
+    my_tree: a tree file, readable by Bio.Phylo.
+    ref_names_ls: a list of reference tip names.
+    verbose: verbosity parameter.
+
+    RETURNS
+    -------
+    df: pandas dataframe with columns: tip_names, their distance to every given
+    reference name, the nearest reference distance, and nearest reference label.
+
+    TODO: implement thresholding by percentile.
+    """
+    t0 = time.time()
+
+    # Get all tipnames
+    names_ls = [x.name for x in my_tree.get_terminals()]
+
+    # Check
+    for ref_nm in ref_names_ls:
+        if ref_nm not in names_ls:
+            print("WARNING: %s not found in input tree!" % ref_nm)
+
+    # Get the non-reference names
+    non_ref_names_ls = list(set.difference(set(names_ls), set(ref_names_ls)))
+
+    if verbose:
+        print("No. of reference tip names = %s" % len(ref_names_ls))
+        print("No. of non-reference tip names = %s" % len(non_ref_names_ls))
+
+    # Compute all possible distances between all tips and all references
+    contents = []
+    for nm in non_ref_names_ls:
+        line = [nm]
+        for ref_nm in ref_names_ls:
+            tt_dist = tip_to_tip_distance(my_tree, nm, ref_nm)
+            line.append(tt_dist)
+
+        contents.append(line)
+
+    col_names = ["tip_name"]
+    for ref_nm in ref_names_ls:
+        col_names.append("dist_to_"+ref_nm)
+
+    df = pd.DataFrame(data=contents,
+                      columns=col_names)
+
+    if verbose:
+        print("Done in %.2fs" % (time.time() - t0))
+
+    df_cols = list(df.columns)[1:]
+    df["clade_label"] = df[df_cols].idxmin(axis=1)
+    df["clade_label"] = df.apply(lambda row: str(row["clade_label"]).replace("dist_to_", ""), axis=1)
+    df["min_dist"] = df.loc[:, df_cols].min(axis=1)
+
+    return df
