be08eed677538c3b449d7e8c35558df051d914c0
Don86 <andreyito90@gmail.com>
2018-01-12 18:48:45 +1100

    Added stuff under development

---
+++ biophylo_utils.py
@@ -0,0 +1,11 @@
+import re
+import copy
+import math
+import datetime as dt
+
+from Bio import Phylo
+
+"""
+A bunch of commonly used utility functions related to Bio.Phylo. Not strictly
+related to baltic3, but I'm putting them here for git saving convenience.
+"""
